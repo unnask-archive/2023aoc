11c5a02b01a2f666865cc35ddbfed3b882a8482b
unnask <8060462+unnask@users.noreply.github.com>
2023-12-13 22:12:47 -0600

    day 2 part 1 complete

--- day02/src/main.zig
+++ day02/src/main.zig
@@ -35,28 +35,70 @@ const Bag = struct {
 fn parseLine(line: []const u8) Bag {
     var idxIter = std.mem.splitScalar(u8, line, ':');
 
-    const game = idxIter.next().?;
-    const lineResults = idxIter.next().?;
+    const trimChars = [_]u8{ ' ', '\n', '\r' };
+    const gameNo = std.mem.trim(u8, idxIter.next().?, &trimChars);
+    const lineResults = std.mem.trim(u8, idxIter.next().?, &trimChars);
 
-    std.debug.print("game: {s}\n line: {s}\n\n", .{ game, lineResults });
-
-    return Bag{
-        .idx = 1,
-        .red = 1,
-        .green = 1,
-        .blue = 1,
+    const gameIndex = blk: {
+        var indexIter = std.mem.splitScalar(u8, gameNo, ' ');
+        _ = indexIter.next();
+        const tmpIndex = indexIter.next().?;
+        break :blk std.fmt.parseInt(i32, tmpIndex, 10) catch 0;
     };
+
+    var bag: Bag = Bag{
+        .idx = gameIndex,
+        .red = 0,
+        .blue = 0,
+        .green = 0,
+    };
+    var gameIter = std.mem.splitScalar(u8, lineResults, ';');
+    while (gameIter.next()) |game| {
+        var pullIter = std.mem.splitScalar(u8, game, ',');
+
+        while (pullIter.next()) |pull| {
+            const trimPull = std.mem.trim(u8, pull, &trimChars);
+            var tmpIter = std.mem.splitScalar(u8, trimPull, ' ');
+
+            const tmpCount = tmpIter.next().?;
+            const colour = tmpIter.next().?;
+
+            const count = std.fmt.parseInt(i32, tmpCount, 10) catch 0;
+            if (std.mem.eql(u8, colour, "red")) {
+                if (count > bag.red) {
+                    bag.red = count;
+                }
+            } else if (std.mem.eql(u8, colour, "blue")) {
+                if (count > bag.blue) {
+                    bag.blue = count;
+                }
+            } else {
+                if (count > bag.green) {
+                    bag.green = count;
+                }
+            }
+        }
+    }
+
+    return bag;
 }
 
 fn performPart1(input: []const u8) !void {
     var linesIter = std.mem.splitScalar(u8, input, '\n');
 
+    var total: i32 = 0;
     while (linesIter.next()) |line| {
         if (line.len < 6) {
             continue;
         }
-        _ = parseLine(line);
+        const bag = parseLine(line);
+
+        if (bag.red <= 12 and bag.green <= 13 and bag.blue <= 14) {
+            total += bag.idx;
+        }
     }
+
+    std.debug.print("The total is: {d}\n", .{total});
 }
 
 pub fn main() !void {
