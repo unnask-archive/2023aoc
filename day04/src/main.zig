095a2b127c897f0e00a7f490a29968d62f1a2c70
Brad Laurie <8060462+unnask@users.noreply.github.com>
2023-12-17 23:56:21 -0600

    much of day 4

--- day04/src/main.zig
+++ day04/src/main.zig
@@ -17,7 +17,8 @@ pub fn main() !void {
 
     const input = try readFile(allocator, "input");
 
-    std.debug.print("{s}\n", .{input});
+    //std.debug.print("{s}\n", .{input});
+    p1Answer(input);
 }
 
 const Card = struct {
@@ -29,16 +30,20 @@ const Card = struct {
     fn parseCard(self: *Self, card: []const u8) void {
         var window = std.mem.window(u8, card, 3, 3);
 
-        for (window.next(), 0..) |num, i| {
+        var i: usize = 0;
+        while (window.next()) |num| {
             self.drawn[i] = std.fmt.parseInt(i32, num, 10) catch 0;
+            i += 1;
         }
     }
 
     fn parseGuessed(self: *Self, guessed: []const u8) void {
         var window = std.mem.window(u8, guessed, 3, 3);
 
-        for (window.next(), 0..) |num, i| {
+        var i: usize = 0;
+        while (window.next()) |num| {
             self.guesses[i] = std.fmt.parseInt(i32, num, 10) catch 0;
+            i += 1;
         }
     }
 };
@@ -47,8 +52,15 @@ fn p1Answer(input: []const u8) void {
     var lnIter = std.mem.splitScalar(u8, input, '\n');
 
     while (lnIter.next()) |line| {
-        const idx = std.mem.indexOf(u8, line, '|');
-        const card = Card.init(input[10..idx], input[idx + 1 ..]);
-        _ = card;
+        const idx = std.mem.indexOf(u8, line, &[_]u8{'|'}) orelse 30;
+        var card = Card{
+            .drawn = .{0} ** 10,
+            .guesses = .{0} ** 25,
+        };
+        card.parseCard(input[10..idx]);
+        card.parseGuessed(input[idx + 1 ..]);
+
+        const count = std.mem.count(i32, &card.drawn, &card.guesses);
+        std.debug.print("Count is: {d}\n", count);
     }
 }
