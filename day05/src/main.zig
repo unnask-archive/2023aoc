d07011d1062a1fbd91d313dcd56a0d8cf31f1e00
Brad Laurie <8060462+unnask@users.noreply.github.com>
2023-12-20 16:24:47 -0600

    get started day 5

--- day05/src/main.zig
+++ day05/src/main.zig
@@ -12,6 +12,43 @@ fn readFile(allocator: Allocator, filename: []const u8) ![]const u8 {
     return try reader.readAllAlloc(allocator, sz);
 }
 
+const Pair = struct {
+    position: usize,
+    range: usize,
+};
+
+const Mapping = struct { from: []const u8, to: []const u8, srcOvrd: []Pair, destOvrd: []Pair };
+
+const Almanac = struct {
+    const MappingType = std.AutoHashMap([]const u8, Mapping);
+    mappings: MappingType,
+};
+
+fn parseInput(input: []const u8) void {
+    var lnIter = std.mem.splitScalar(u8, input, '\n');
+
+    const seeds = lnIter.next();
+    _ = seeds;
+    while (lnIter.next()) |line| {
+        const trimmed = std.mem.trim(u8, line, &[_]u8{ ' ', '\n' });
+        if (trimmed.len == 0) {
+            continue;
+        }
+
+        if (std.ascii.isDigit(trimmed[0])) {
+            // new mapping section
+            var mtIter = std.mem.splitSequence(u8, trimmed, "-to-");
+            const from = mtIter.next();
+            _ = from;
+            const to = mtIter.next();
+            _ = to;
+            continue;
+        }
+
+        // parse the numbers
+    }
+}
+
 pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     var allocator = gpa.allocator();
