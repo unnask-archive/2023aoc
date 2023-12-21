42d46c981df93b65a7b84f2d2bdf8159d8976e62
unnask <8060462+unnask@users.noreply.github.com>
2023-12-20 21:39:17 -0600

    more work on day 5

--- day05/src/main.zig
+++ day05/src/main.zig
@@ -12,49 +12,80 @@ fn readFile(allocator: Allocator, filename: []const u8) ![]const u8 {
     return try reader.readAllAlloc(allocator, sz);
 }
 
-const Pair = struct {
-    position: usize,
+const Triple = struct {
+    source: usize,
+    dest: usize,
     range: usize,
 };
 
-const Mapping = struct { from: []const u8, to: []const u8, srcOvrd: []Pair, destOvrd: []Pair };
+const Mapping = struct { from: []const u8, to: []const u8, map: []Triple };
 
 const Almanac = struct {
-    const MappingType = std.AutoHashMap([]const u8, Mapping);
+    const MappingType = std.StringHashMap(Mapping);
+    const Self = @This();
     mappings: MappingType,
-};
 
-fn parseInput(input: []const u8) void {
-    var lnIter = std.mem.splitScalar(u8, input, '\n');
+    fn fromInput(allocator: Allocator, input: []const u8) !Almanac {
+        var sources = std.ArrayList(Triple).init(allocator);
+        var lnIter = std.mem.splitScalar(u8, input, '\n');
 
-    const seeds = lnIter.next();
-    _ = seeds;
-    while (lnIter.next()) |line| {
-        const trimmed = std.mem.trim(u8, line, &[_]u8{ ' ', '\n' });
-        if (trimmed.len == 0) {
-            continue;
+        var ret = Almanac{
+            .mappings = MappingType.init(allocator),
+        };
+
+        while (lnIter.next()) |line| {
+            const trimmed = std.mem.trim(u8, line, &[_]u8{ ' ', '\n' });
+            if (trimmed.len == 0) {
+                continue;
+            }
+
+            if (!std.ascii.isDigit(trimmed[0])) {
+                // new mapping section
+                var mtIter = std.mem.splitSequence(u8, trimmed, "-to-");
+                const from = mtIter.next().?;
+                const to = mtIter.next().?;
+
+                while (lnIter.next()) |map| {
+                    const tln = std.mem.trim(u8, map, &[_]u8{'\n'});
+                    if (tln.len == 0) {
+                        break;
+                    }
+
+                    var tlnIter = std.mem.splitScalar(u8, tln, ' ');
+                    const srcstr = tlnIter.next().?;
+                    const deststr = tlnIter.next().?;
+                    const rangestr = tlnIter.next().?;
+
+                    const triple = Triple{
+                        .source = std.fmt.parseUnsigned(usize, srcstr, 10) catch 0,
+                        .dest = std.fmt.parseUnsigned(usize, deststr, 10) catch 0,
+                        .range = std.fmt.parseUnsigned(usize, rangestr, 10) catch 0,
+                    };
+                    try sources.append(triple);
+                }
+
+                const mapping = Mapping{
+                    .to = to,
+                    .from = from,
+                    .map = try sources.toOwnedSlice(),
+                };
+                try ret.mappings.put(mapping.to, mapping);
+            }
         }
-
-        if (std.ascii.isDigit(trimmed[0])) {
-            // new mapping section
-            var mtIter = std.mem.splitSequence(u8, trimmed, "-to-");
-            const from = mtIter.next();
-            _ = from;
-            const to = mtIter.next();
-            _ = to;
-            continue;
-        }
-
-        // parse the numbers
+        return ret;
     }
-}
+};
 
 pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     var allocator = gpa.allocator();
 
     const input = try readFile(allocator, "input");
-    for (input) |char| {
-        std.debug.print("{any}", .{char});
-    }
+    const idx = std.mem.indexOfScalar(u8, input, '\n') orelse 0;
+
+    const seeds = input[0..idx];
+    _ = seeds;
+
+    const almanac = try Almanac.fromInput(allocator, input[idx + 1 ..]);
+    _ = almanac;
 }
