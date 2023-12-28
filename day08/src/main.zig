2f715ab3b425fd6ba0620744098347f0d3c146a7
unnask <8060462+unnask@users.noreply.github.com>
2023-12-28 01:00:03 -0600

    Day 8

--- day08/src/main.zig
+++ day08/src/main.zig
@@ -11,6 +11,8 @@ fn readFile(allocator: Allocator, filename: []const u8) ![]u8 {
     return reader.readAllAlloc(allocator, sz);
 }
 
+const Map = std.StringHashMap([]const u8);
+
 pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     var allocator = gpa.allocator();
@@ -22,7 +24,6 @@ pub fn main() !void {
     const moves = secIter.next().?;
     const map = secIter.next().?;
 
-    const Map = std.StringHashMap([]const u8);
     var lookup = std.AutoHashMap(u8, Map).init(allocator);
     var left = Map.init(allocator);
     var right = Map.init(allocator);
@@ -32,6 +33,8 @@ pub fn main() !void {
         lookup.deinit();
     }
 
+    var p2List = std.ArrayList([]const u8).init(allocator);
+    defer p2List.deinit();
     var lineIter = std.mem.splitScalar(u8, map, '\n');
     while (lineIter.next()) |line| {
         if (line.len == 0) {
@@ -39,6 +42,9 @@ pub fn main() !void {
         }
         var mapIter = std.mem.splitSequence(u8, line, " = ");
         const val = mapIter.next().?;
+        if (val[2] == 'A') {
+            try p2List.append(val);
+        }
         var lr = mapIter.next().?;
         lr = lr[1 .. lr.len - 1];
 
@@ -49,19 +55,7 @@ pub fn main() !void {
     try lookup.put('L', left);
     try lookup.put('R', right);
 
-    var total: usize = 0;
-    var element: []const u8 = "AAA";
-    found: while (true) {
-        for (moves) |move| {
-            total += 1;
-            const m = lookup.get(move).?;
-            element = m.get(element).?;
-            if (std.mem.eql(u8, "ZZZ", element)) {
-                std.debug.print("Found it\n", .{});
-                break :found;
-            }
-        }
-    }
+    var total = indexOf(moves, &lookup, "AAA", "ZZZ");
     std.debug.print("Part 1 Total is: {d}\n", .{total});
 
     //part 2 is a bit more annoying
@@ -69,4 +63,55 @@ pub fn main() !void {
     //the elements land on an element ending is Z at the same time.
     //I think we can just find the lowest count of each element, then find the
     //lowest common multiple of the elements instead
+    var lowList = std.ArrayList(usize).init(allocator);
+    defer lowList.deinit();
+    while (p2List.items.len > 0) {
+        try lowList.append(indexOf2(moves, &lookup, p2List.pop()));
+    }
+    //while (lowList.items.len > 0) {
+    //    std.debug.print("{d}\n", .{lowList.pop()});
+    //}
+    const nums = try lowList.toOwnedSlice();
+    defer allocator.free(nums);
+    std.debug.print("Part 2 is {d}\n", .{leastCommonMultiple(nums)});
+}
+
+fn indexOf(moves: []const u8, lookup: *std.AutoHashMap(u8, Map), st: []const u8, ed: []const u8) usize {
+    var total: usize = 0;
+    var element: []const u8 = st;
+    found: while (true) {
+        for (moves) |move| {
+            total += 1;
+            const m = lookup.get(move).?;
+            element = m.get(element).?;
+            if (std.mem.eql(u8, ed, element)) {
+                break :found;
+            }
+        }
+    }
+    return total;
+}
+
+fn leastCommonMultiple(nums: []const usize) usize {
+    var ret: usize = 1;
+    for (nums) |num| {
+        ret = (ret * num) / std.math.gcd(num, ret);
+    }
+    return ret;
+}
+
+fn indexOf2(moves: []const u8, lookup: *std.AutoHashMap(u8, Map), st: []const u8) usize {
+    var total: usize = 0;
+    var element: []const u8 = st;
+    found: while (true) {
+        for (moves) |move| {
+            total += 1;
+            const m = lookup.get(move).?;
+            element = m.get(element).?;
+            if (element[2] == 'Z') {
+                break :found;
+            }
+        }
+    }
+    return total;
 }
