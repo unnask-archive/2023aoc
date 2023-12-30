4e468014db8456a340ee5e6f35275afb6f0ec55b
unnask <8060462+unnask@users.noreply.github.com>
2023-12-30 04:28:06 -0600

    use embed file instead

--- day09/src/main.zig
+++ day09/src/main.zig
@@ -1,19 +1,4 @@
 const std = @import("std");
-const Allocator = std.mem.Allocator;
+const input = @embedFile("input");
 
-fn readFile(allocator: Allocator, filename: []const u8) ![]u8 {
-    var file = try std.fs.cwd().openFile(filename, .{});
-
-    const sz = (try file.stat()).size;
-    var br = std.io.bufferedReader(file.reader());
-    var reader = br.reader();
-    return try reader.readAllAlloc(allocator, sz);
-}
-
-pub fn main() !void {
-    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
-    var allocator = gpa.allocator();
-
-    const input = try readFile("input");
-    defer allocator.free(input);
-}
+pub fn main() !void {}
