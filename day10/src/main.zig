375bb7d27fd6ec3ec5300868eb258e5a58a2e1d0
unnask <8060462+unnask@users.noreply.github.com>
2024-01-02 00:05:16 -0600

    basic 10 written, not tested

--- day10/src/main.zig
+++ day10/src/main.zig
@@ -1,9 +1,59 @@
 const std = @import("std");
-const input = @embedFile("example");
+const in = @embedFile("example");
+
+fn findLoop(input: []const u8, lsz: usize, idx: usize, prev: usize) usize {
+    switch (input[idx]) {
+        '|' => {
+            var vert = idx + lsz;
+            if (prev > idx) {
+                vert = idx - lsz;
+            }
+            return 1 + findLoop(input, lsz, vert, idx);
+        },
+        '-' => {
+            var horiz = idx + 1;
+            if (prev > idx) {
+                horiz = idx - 1;
+            }
+            return 1 + findLoop(input, lsz, horiz, idx);
+        },
+        'L' => {
+            var tmp = idx + 1;
+            if (prev > idx) {
+                tmp = idx - lsz;
+            }
+            return 1 + findLoop(input, lsz, tmp, idx);
+        },
+        'J' => {
+            var tmp = idx - 1;
+            if (prev == idx - 1) {
+                tmp = idx - lsz;
+            }
+            return 1 + findLoop(input, lsz, tmp, idx);
+        },
+        '7' => {
+            var tmp = idx - 1;
+            if (prev == idx - 1) {
+                tmp = idx + lsz;
+            }
+            return 1 + findLoop(input, lsz, tmp, idx);
+        },
+        'F' => {
+            var tmp = idx + 1;
+            if (prev == idx + 1) {
+                tmp = idx + lsz;
+            }
+            return 1 + findLoop(input, lsz, tmp, idx);
+        },
+        else => return 0,
+    }
+}
 
 pub fn main() !void {
-    const ll = std.mem.indexOfScalar(u8, input, '\n') + 1;
-    _ = ll;
-    const st = std.mem.indexOfScalar(u8, input, 'S');
-    _ = st;
+    const ll = std.mem.indexOfScalar(u8, in, '\n').? + 1;
+    const st = std.mem.indexOfScalar(u8, in, 'S').?;
+
+    //actually need to check each possible direction from start
+    const val = findLoop(in, ll, st, 0);
+    _ = val;
 }
