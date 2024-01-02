d1203431ca62135835249d4e54bf8d1cb11f7fbe
unnask <8060462+unnask@users.noreply.github.com>
2024-01-01 21:31:45 -0600

    start

--- day10/src/main.zig
+++ day10/src/main.zig
@@ -1,3 +1,9 @@
 const std = @import("std");
+const input = @embedFile("example");
 
-pub fn main() !void {}
+pub fn main() !void {
+    const ll = std.mem.indexOfScalar(u8, input, '\n') + 1;
+    _ = ll;
+    const st = std.mem.indexOfScalar(u8, input, 'S');
+    _ = st;
+}
