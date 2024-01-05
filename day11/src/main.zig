8686ae5df1e9efeeaae5a0465551bf3c66b508f7
unnask <8060462+unnask@users.noreply.github.com>
2024-01-05 00:01:09 -0600

    start day 11

--- day11/src/main.zig
+++ day11/src/main.zig
@@ -1,13 +1,55 @@
 const std = @import("std");
+const Allocator = std.mem.Allocator;
 const in = @embedFile("example");
+
+const Coord = struct { x: usize, y: usize };
+
+// for the others, I didn't do this, but tracking rows and columns with many
+// extra \n was proving annoying. so split it I shall
+fn splitInput(allocator: Allocator, input: []const u8, by: u8) ![][]const u8 {
+    var list = std.ArrayList([]const u8).init(allocator);
+    var iter = std.mem.splitScalar(u8, input, by);
+
+    while (iter.next()) |section| {
+        if (section.len == 0) {
+            continue;
+        }
+        try list.append(section);
+    }
+
+    return try list.toOwnedSlice();
+}
 
 pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     var allocator = gpa.allocator();
-    const icols = std.mem.indexOfScalar(u8, in, '\n').?;
-    const irows = in.len / icols;
+    comptime var icols = std.mem.indexOfScalar(u8, in, '\n').?;
+    comptime var irows = in.len / (icols + 1);
 
-    const wkmem = ((icols - 1) * 2) * (irows * 2); // rows and cols will double at most
-    var storage = try allocator.alloc(u8, wkmem);
-    _ = storage;
+    var grid = try splitInput(allocator, in, '\n');
+    defer allocator.free(grid);
+
+    var er: [irows]bool = .{true} ** irows;
+    var ec: [icols]bool = .{true} ** icols;
+    var coords = std.ArrayList(Coord).init(allocator);
+    defer coords.deinit();
+
+    for (grid, 0..) |line, r| {
+        for (line, 0..) |ch, c| {
+            std.debug.print("{c}", .{ch});
+
+            if (ch == '#') {
+                er[r] = false;
+                ec[c] = false;
+                try coords.append(Coord{
+                    .x = r,
+                    .y = c,
+                });
+            }
+        }
+        std.debug.print("\n", .{});
+    }
+
+    var storage = allocator.alloc(u8, (irows * 2) * (icols * 2));
+    defer allocator.free(storage);
 }
