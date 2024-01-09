6dcead56b1f3319ecf513288bd8016f9d7f9f178
unnask <8060462+unnask@users.noreply.github.com>
2024-01-08 22:06:07 -0600

    notes

--- day12/src/main.zig
+++ day12/src/main.zig
@@ -3,4 +3,8 @@ const Allocator = std.mem.Allocator;
 
 const in = @embedFile("example");
 
+//Looks like the intended solution here is dynamic programming,
+//This was a learning process, so might as well learn more about
+//the desired solution rather than brute forcing it.
+
 pub fn main() !void {}
